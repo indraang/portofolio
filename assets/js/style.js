6c7f28eb2222988cc9dc33112867c87eea301272
Indra <fansmirage@gmail.com>
2018-10-20 09:49:26 +0700

    commit pertama

---
+++ assets/js/style.js
@@ -0,0 +1,11 @@
+
+	if($("#typed-text").length > 0){
+		var typed2 = new Typed("#typed-text",{
+			strings:["I'AM INDRA","AANG","FANANI"],
+			typeSpeed:10,
+			loop:true,
+			backDelay:2000
+		})
+	}
+
+
